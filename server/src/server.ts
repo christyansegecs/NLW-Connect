a3400e2ee52e44e231f6354412406eb017646071
christyansegecs <christyansegecs@hotmail.com>
2025-04-20 19:19:56 -0300

    Aula 2 Node

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,7 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { env } from './env'
+import { accessInviteLinkRoute } from './routes/access-invite-link-route'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -38,6 +39,7 @@ app.register(fastifySwaggerUi, {
 })
 
 app.register(subscribeToEventRoute)
+app.register(accessInviteLinkRoute)
 
 app.get('/hello', () => {
   return 'Hello World!'
