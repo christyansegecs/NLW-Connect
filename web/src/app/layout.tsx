a06ef3abc5d4162a868e4fb5277d55d0696a1e7f
christyansegecs <christyansegecs@hotmail.com>
2025-04-21 21:05:56 -0300

    Aula 2 - React

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from 'next';
-import { Montserrat, Oxanium } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'NLW Connect',
-};
+}
 
 const oxanium = Oxanium({
   weight: ['500', '600'],
@@ -21,13 +21,15 @@ const montserrat = Montserrat({
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang='en' className={`${oxanium.variable} ${montserrat.variable}`}>
+    <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
       <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat md:bg-right-top bg-top">
-        {children}
+        <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+          {children}
+        </main>
       </body>
     </html>
-  );
+  )
 }
